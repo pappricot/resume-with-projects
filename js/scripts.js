98a1bef4a5744e07f4ee0f07d5edf845b9f04632
Anya <anya@Anyas-MacBook-Pro.local>
2018-10-01 16:41:51 -0700

    dynamic header span

--- js/scripts.js
+++ js/scripts.js
@@ -25,5 +25,64 @@ jQuery(function ($) {
         });
     }());
 
+    const messages = [
+        'Programmer',
+        'Doodler',
+        'Creator'
+    ]
+
+    let currentMessageIndex = 0;
+
+    const printMessage = (message) => {
+        for (let i=0; i< message.length; i++) {
+            const text = message.substring(0, i+1)
+            setTimeout(() => {
+                $('.name-wrapper span').text(text);
+            }, i*250)
+            
+        }
+    }
+
+    const changeMessage = () => {
+        printMessage(messages[currentMessageIndex]);
+        currentMessageIndex = (currentMessageIndex+1)%3
+    }
+
+    setInterval(changeMessage, 2500)
+   
+    setTimeout(()=> {
+        $('.progress-bar#1').width('100%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#2').width('90%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#3').width('75%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#4').width('55%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#5').width('75%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#6').width('60%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#7').width('75%')
+    }, 3000)
+    setTimeout(()=> {
+        $('.progress-bar#8').width('90%')
+    }, 3000)
+
+//    $('.progress-wrapper').scrollspy({
+//        onEnter: function onEnter(element) {
+//            console.log('entered')
+//         $('.progress-bar#1').css('width', '100%')
+//        },
+//        onLeave: function onLeave(element) {
+//         $('.progress-bar#1').css('width', '0%')
+//        }
+//    })
 
 }); // JQuery end
